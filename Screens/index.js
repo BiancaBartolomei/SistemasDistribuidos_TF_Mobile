bfac21e52223a417c7fb2151e01721be8540c77c
Bianca <biancarbartolomei@gmail.com>
2020-06-08 13:43:29 -0300

    [FIX] StackNavigator

--- Screens/index.js
+++ Screens/index.js
@@ -1,7 +1,9 @@
 import Login from './Login';
 import CreateUser from './CreateUser';
 
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+
+import { createStackNavigator } from 'react-navigation-stack';
 
 const Routes = createAppContainer(
   createStackNavigator({
