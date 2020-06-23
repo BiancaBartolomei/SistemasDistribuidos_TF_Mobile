a9ab96a3e4746cb9d38ab114095247810dca049d
Bianca <biancarbartolomei@gmail.com>
2020-06-23 15:47:53 -0300

    [FIX] Main com headless

--- Screens/Main.js
+++ Screens/Main.js
@@ -5,8 +5,47 @@ import PlaceItem from '../Components/PlaceItem'
 
 import React, {Component} from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Image, TextInput, FlatList, ImageBackground } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';  
 
-class Main extends Component {
+
+
+
+
+const LogLocation = async (id) => {
+
+  Geolocation.getCurrentPosition(info => {
+    fetch('http://192.168.15.14:3300/sendLocation',{
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user_id: id,
+        lat: info["coords"]["latitude"],
+        long: info["coords"]["longitude"]
+      })
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log("Foi: "+info);
+      })
+      .catch((error) => {
+         console.error(error);
+      });
+
+  });
+
+  setInterval(()=>{LogLocation(id)}, 600000)
+
+};
+
+
+
+AppRegistry.registerHeadlessTask('LogLocation', () => LogLocation());
+
+
+export default class Main extends Component {
   constructor(props) {
     super(props);
 
@@ -33,6 +72,9 @@ class Main extends Component {
   }
 
   componentDidMount(){
+    var id = this.state.user.user_id
+    this.timer = LogLocation(id);
+
     const { navigation } = this.props;
     this.getFavourites()
     this.navFocusListener = navigation.addListener('didFocus', () => {
